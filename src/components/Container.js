8ed22bdc1788628ee16df525c3d3ce32464bece0
verakissyou <veronicaciuca17@gmail.com>
2024-12-24 09:12:54 +0200

    Change the role value for the container

--- src/components/Container.js
+++ src/components/Container.js
@@ -5,7 +5,7 @@ import { StyledContainer } from './styles/Container.styled.js';
 
 function Container() {
   return (
-    <StyledContainer role='page'>
+    <StyledContainer role='This the container that holds all the project'>
       <Header />
       <Main />
       <Footer />
